94e69d8a2cd46aa850d03db4afd2bcb8b0af4608
stjs-code <tiznadosergio34@gmail.com>
2026-02-25 20:23:54 -0300

    Estructura basica de controladores + Configuracion de PostgreSQL + Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Este es un pequeño experimento que aun no esta terminado,
+en este busco experimentar creando un CRUD basico para usuarios
+en una aplicacion con SpringBoot usando PostgreSQL.
---
+++ src/main/java/com/example/demo/Controladores/Controllers.java
@@ -0,0 +1,36 @@
+package com.example.demo.Controladores;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@RestController
+@RequestMapping("/api")
+public class Controllers {
+
+    @PostMapping("/")
+    public ResponseEntity<?> Post(@RequestBody String entity) {
+        
+        return ResponseEntity.ok("Guardado");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> GetAll() {
+        return ResponseEntity.ok("Todos los usuarios");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> Get(@PathVariable Long id) {
+        return ResponseEntity.ok("Usuario independiente");
+    }
+    
+}
